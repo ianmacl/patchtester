3d6b595b40a04c23203ab8e670f6d9fd14b28a94
Ian MacLennan <ian.maclennan@joomla.org>
2011-10-12 23:24:33 -0400

    Fixed manifest file. Fixed uninstall sql file. /me hangs head in shame Added build file Added github file manifest Added package manifest Added update files

---
+++ patchtester/build.sh
@@ -0,0 +1,17 @@
+rm -rf site
+rm -rf admin
+rm -rf ../patchtester.tar.bz2
+cp -r ../administrator/components/com_patchtester admin
+cp -r ../components/com_patchtester site
+mv admin/patchtester.xml patchtester.xml
+tar jcf ../com_patchtester.tar.bz2 site admin patchtester.xml
+
+rm -rf github
+mkdir github
+cp ../libraries/joomla/client/github.php github
+cp ../libraries/joomla/client/githubobject.php github
+cp -r ../libraries/joomla/client/github github
+cp github.xml github
+tar jcf ../file_github.tar.bz2 github/*
+
+tar jcf ../pkg_patchtester.tar.bz2 pkg_patchtester.xml ../com_patchtester.tar.bz2 ../file_github.tar.bz2
